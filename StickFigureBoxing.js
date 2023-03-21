48675ee9297dbef00e5606402b8c91f69ab1b243
August M <69694598+AugustMajtenyi@users.noreply.github.com>
2023-03-20 22:12:12 -0400

    Update StickFigureBoxing.js

--- StickFigureBoxing.js
+++ StickFigureBoxing.js
@@ -55,9 +55,9 @@ function preload() {
         
     //let canvas = document.getElementById("myCanvas");
     //let p5Canvas = createCanvas(canvas.width, canvas.height);
-     let canvas = createCanvas(800, 500);
-     canvas.parent("myCanvas"); // Link the canvas to the <canvas> element in the HTML
-      
+     //let canvas = createCanvas(800, 500);
+     //canvas.parent("myCanvas"); // Link the canvas to the <canvas> element in the HTML
+      createCanvas(800, 500).parent("myCanvas")
     //createCanvas(800, 500);
     //canvas.style = "position:absolute; left: 50%; width: 800px; margin-left: -400px; margin-top: 500px";
     createButtons() 
